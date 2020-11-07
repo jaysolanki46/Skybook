922424f2f3d0ece32f374e0b963c5c4113a33824
jay solanki <jaysolanki46@gmail.com>
2020-11-08 11:59:42 +1300

    Site snaps uploaded

--- src/db/DBConfig.java
+++ src/db/DBConfig.java
@@ -9,14 +9,14 @@ public class DBConfig {
 	public static Connection connection() throws ClassNotFoundException, SQLException {
 		 
 		// For production
-		Class.forName("org.mariadb.jdbc.Driver");
-		Connection connection = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/skybook?useSSL=false", "root", "");
+		//Class.forName("org.mariadb.jdbc.Driver");
+		//Connection connection = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/skybook?useSSL=false", "root", "");
 		
 		
 		// For test
-		/* Class.forName("com.mysql.jdbc.Driver");
-		 Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/skybook?useSSL=false", "root", "Js0322!@");
-		*/ 
+		 Class.forName("com.mysql.jdbc.Driver");
+		 Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/skybook?useSSL=false", "root", "XXX@");
+		 
 		 
 		 return connection;
 		 
